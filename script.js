a8eb171c4ca1791dc9470bcbea5c97b3ece1d176
Emily Travers <embtravers@gmail.com>
2022-08-21 15:09:49 -0400

    adding and subtracting price works

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 //establish your moneys and cart total as 0
 let priceD = 0;
 let priceVIP = 0;
-//let moneyD = prompt("How many Dolluhs do you have?");
-//let moneyVIP = prompt("How many VIP Points do you have?");
+let moneyD = prompt("How many Dolluhs do you have?");
+let moneyVIP = prompt("How many VIP Points do you have?");
 
 window.onload = function(){
 document.querySelector('.currentD').innerHTML = `Your current Dolluhs: ${moneyD}`
@@ -22,16 +22,42 @@ function toggleActive(element){
     element.classList.toggle('active')
 }
 
-//update DOM for price
-function updatePrice(){
-    
+function addOrSubctract(element){
+    if (element.classList.contains('active')){
+      return 1
+    }
+    else
+    {
+      return -1
+    }
+}
+
+function checkCurrency(element){
+    if (element.classList.contains('dolluhs')){
+        return 'priceD'
+    }
+    else
+    {
+        return 'priceVIP'
+    }
 }
 
 //needs to return the price at the end of the string
 function findPrice(element){
     const endpoint = element.innerText.search(':');
     //console.log(endpoint);
-    return element.innerText.slice(endpoint)
+    return element.innerText.slice(endpoint + 2)
+}
+
+function updateSpent(currency, multiplier, price){
+    if (currency == "priceD"){
+        priceD = priceD + (multiplier * price)
+        document.querySelector('.spentD').innerText = `Current Dolluhs spent: ${priceD}`
+    }
+    else{
+        priceVIP = priceVIP + (multiplier * price)
+        document.querySelector('.spentVIP').innerText = `Current VIP Points spent: ${priceVIP}`
+    }
 }
 
 //create an array of all list items based on class of list-group-item
@@ -41,6 +67,16 @@ const items = document.querySelectorAll('.list-group-item');
 items.forEach(element => {
     element.addEventListener('click', function handleClick(){
        toggleActive(element);
-       findPrice(element);
+       //pick if updating priceD or priceVIP
+       let currency = checkCurrency(element);
+       //console.log(currency);
+       //pick if adding or subtracting
+       let multiplier = addOrSubctract(element);
+       //console.log(multiplier);
+       //cast the price as a float 
+       let price = parseFloat(findPrice(element))
+       //console.log(price);
+       //console.log(currency)
+       updateSpent(currency, multiplier, price);
     });
 });
